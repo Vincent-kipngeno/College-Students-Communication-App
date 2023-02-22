f086160597b6fa1c1291c5e4c03d9b14a1a68a93
vincent-kipngeno <kipngenovi2001@gmail.com>
2023-02-22 17:19:27 +0300

    Add functionality to check if a user is checked

--- app/src/main/java/com/example/college_students_communication_app/models/User.java
+++ app/src/main/java/com/example/college_students_communication_app/models/User.java
@@ -1,7 +1,12 @@
 package com.example.college_students_communication_app.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     public String uid, email, username, phone;
+    public boolean isChecked;
+    public ArrayList<String> groups;
 
     public User(){
 
@@ -12,6 +17,16 @@ public class User {
         this.email = email;
         this.username = username;
         this.phone = phone;
+        this.isChecked = false;
+    }
+
+    public User(String uid, String email, String username, String phone, ArrayList<String> groups) {
+        this.uid = uid;
+        this.email = email;
+        this.username = username;
+        this.phone = phone;
+        this.isChecked = false;
+        this.groups = groups;
     }
 
     public String getUid() {
@@ -44,5 +59,21 @@ public class User {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+
+    public ArrayList<String> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(ArrayList<String> groups) {
+        this.groups = groups;
     }
 }
